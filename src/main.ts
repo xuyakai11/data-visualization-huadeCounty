61ffedca5de07e45ee5afb527df66ce274ea2cd8
xuyakai <xuyakai@chuangxin.com>
2019-11-25 16:31:43 +0800

    接口联调

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import './axios.tool' // 导入封装好的axios
 import adapt from './assets/adapt'
-import './plugins/element.js'
+// import './plugins/element.js'
 
 Vue.config.productionTip = false // 设置为 false 以阻止 vue 在启动时生成生产提示
 
